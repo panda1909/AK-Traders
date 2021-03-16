15a989a92d19c9e656c9c100be7178b9d59564d4
Muhammad Umair <Umairmuhammad433@gmail.com>
2021-03-16 15:28:38 +0500

    nothing

--- AK_Traders/settings.py
+++ AK_Traders/settings.py
@@ -11,13 +11,12 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'yawxyikwpxc+m2p3vu(f!rd*9hq&@dl-lg7hr7m@l%fooxc&63'
@@ -119,3 +118,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
